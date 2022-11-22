010d6e4660653b7a34e2da5bfd61679702c45664
Zak Rodgers <psyzr3@nottingham.ac.uk>
2022-11-22 09:39:43 +0000

    move data to CW2

--- CWPT2/Cw_part2.py
+++ CWPT2/Cw_part2.py
@@ -5,12 +5,14 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-df_raw_data = pd.read_csv("trainDataset.csv")
-df_test_data = pd.read_csv("testDatasetExample.csv")
-
-cleanData(df_raw_data)
+def main():
+    df_raw_data = pd.read_csv("trainDataset.csv")
+    df_test_data = pd.read_csv("testDatasetExample.csv")
+    
+    cleanData(df_raw_data)
 
 def cleanData(df_raw):
     print(df_raw.duplicated().sum())
     
+if __name__ == "__main__":
+    main()
