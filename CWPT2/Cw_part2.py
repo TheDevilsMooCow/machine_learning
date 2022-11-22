734278b15d4bee484e98d16351902e59c4e25f7f
Zak Rodgers <psyzr3@nottingham.ac.uk>
2022-11-22 10:09:10 +0000

    replace 999 with nan

--- CWPT2/Cw_part2.py
+++ CWPT2/Cw_part2.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def main():
-    df_raw_data = pd.read_csv("trainDataset.csv")
-    df_test_data = pd.read_csv("testDatasetExample.csv")
+    df_raw_data = pd.read_excel('trainDataset.xls')
+    df_test_data = pd.read_excel('testDatasetExample.xls')
     
     cleanData(df_raw_data)
 
 def cleanData(df_raw):
     print(df_raw.duplicated().sum())
+    df_raw = df_raw.replace(['999'], 'NaN')
+    
     
 if __name__ == "__main__":
     main()
